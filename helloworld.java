dbc11c231e9069f8b3204f4f321b9dabf6af3bc6
adanirbandalui <adanirbandalui@gmail.com>
2026-04-20 09:15:50 +0530

    first commit

---
+++ helloworld.java
@@ -0,0 +1,23 @@
+/**
+ * A simple HelloWorld application demonstrating Java basics.
+ * This program outputs a greeting message to the console.
+ * 
+ * @author Developer
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * Main entry point of the application.
+     * 
+     * @param args Command line arguments (not used in this example)
+     */
+    public static void main(String[] args) {
+        // Display a greeting message
+        System.out.println("Hello, World!");
+        
+        // Optional: Add some additional output for better formatting
+        System.out.println("Welcome to Java Programming!");
+        System.out.println("=====================================");
+    }
+}
